30674b56b95787212a6a25aca075127fd756b8e3
Antyradek <antyradek@gmail.com>
2016-11-19 18:28:08 +0100

    Ustaw prawidłowe osie

--- plugins/omnivelma.cpp
+++ plugins/omnivelma.cpp
@@ -24,17 +24,17 @@ public:
         //podłączenie do wydarznia aktualizacji
         this -> updateConnection = event::Events::ConnectWorldUpdateBegin(std::bind(&Omnivelma::OnUpdate, this, std::placeholders::_1));
 
-		//common::Logger logger("Omnivelma", common::Color::Purple.GetAsARGB(), common::Logger::LogType::STDOUT);
-		//logger("Podłączono wtyczkę", 10);
+        //common::Logger logger("Omnivelma", common::Color::Purple.GetAsARGB(), common::Logger::LogType::STDOUT);
+        //logger("Podłączono wtyczkę", 10);
 
-		std::cout << "Podłączono wtyczkę do " << model -> GetName() << std::endl;
-		linkPrefix = MODEL_NAME.append("::").append(model -> GetName()).append("::");
+        std::cout << "Podłączono wtyczkę do " << model -> GetName() << std::endl;
+        linkPrefix = MODEL_NAME.append("::").append(model -> GetName()).append("::");
 
-		jointController = model -> GetJointController();
-		joints = jointController -> GetJoints();
+        jointController = model -> GetJointController();
+        joints = jointController -> GetJoints();
 
-		std::cout << "Przeguby:" << std::endl;
-		for(auto& x : joints)
+        std::cout << "Przeguby:" << std::endl;
+        for(auto& x : joints)
         {
             std:: cout << x.first << std::endl;
         }
@@ -42,33 +42,31 @@ public:
     }
 
 private:
-  void setRollAxis(std::string const& wheelLinkName, std::string const& rollerJointName, double rotationSide)
-  {
-    // Get the wheel's axis in the world frame.
-    math::Pose const& wheel_world_pose = model->GetLink(wheelLinkName.c_str())->GetWorldPose();
-    math::Matrix3 wheel_world_mat3 = wheel_world_pose.rot.GetAsMatrix3();
-    math::Vector3 wheel_world_y(wheel_world_mat3[0][1], wheel_world_mat3[1][1], wheel_world_mat3[2][1]);
+    void setRollAxis(std::string const& wheelLinkName, std::string const& rollerJointName, double rotationSide)
+    {
+        physics::LinkPtr baseLink = model -> GetLink(linkPrefix + "base");
 
-    // Compute the new roll axis (rotate the wheel axis 45° around the world Z).
-    math::Quaternion rot(math::Vector3(0.0, 0.0, 1.0), rotationSide * math::Angle::Pi.Radian() / 4.0);
-    math::Vector3 newAxis_world = rot.RotateVector(wheel_world_y);
-    math::Vector3 newAxis_local = wheel_world_pose.rot.RotateVectorReverse(newAxis_world);
+        math::Vector3 baseUp = baseLink -> GetWorldPose().rot.GetZAxis();
+        math::Quaternion globalRollerRot = math::Quaternion(baseUp, rotationSide);
+        math::Vector3 wheelRight = model -> GetLink(wheelLinkName) -> GetWorldPose().rot.GetXAxis();
+        double wheelRoll = model -> GetLink(wheelLinkName) -> GetRelativePose().rot.GetRoll();
+        math::Quaternion invWheelRot = math::Quaternion(wheelRight, -wheelRoll);
 
-    // Set the new axis.
-    model->GetJoint(rollerJointName.c_str())->SetAxis(0, newAxis_local);
-
-
-  }
+        math::Vector3 rollerVect = baseLink -> GetWorldPose().rot.GetYAxis();
+        rollerVect = globalRollerRot.RotateVector(rollerVect);
+        rollerVect = invWheelRot.RotateVector(rollerVect);
+        model -> GetJoint(rollerJointName) -> SetAxis(0, rollerVect);
+    }
 
 private:
-  ///Funkcja podłączana do zdarzenia aktualizacji
-  void OnUpdate(const common::UpdateInfo& info)
-  {
-    setRollAxis(linkPrefix + "virtual_wheel_fl",  linkPrefix + "roller_fl",   1.0);
-    setRollAxis(linkPrefix + "virtual_wheel_rl",   linkPrefix + "roller_rl",   -1.0);
-    setRollAxis(linkPrefix + "virtual_wheel_rr",  linkPrefix + "roller_rr",   1.0);
-    setRollAxis(linkPrefix + "virtual_wheel_fr", linkPrefix + "roller_fr", -1.0);
-  }
+    ///Funkcja podłączana do zdarzenia aktualizacji
+    void OnUpdate(const common::UpdateInfo& info)
+    {
+        setRollAxis(linkPrefix + "virtual_wheel_fl", linkPrefix + "roller_fl", math::Angle::Pi.Radian() * 0.75);
+        setRollAxis(linkPrefix + "virtual_wheel_rl", linkPrefix + "roller_rl", math::Angle::Pi.Radian() * 0.25);
+        setRollAxis(linkPrefix + "virtual_wheel_rr", linkPrefix + "roller_rr", -math::Angle::Pi.Radian() * 0.25);
+        setRollAxis(linkPrefix + "virtual_wheel_fr", linkPrefix + "roller_fr", -math::Angle::Pi.Radian() * 0.75);
+    }
 
 private:
     ///Wskaźnik na model
