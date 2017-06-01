ba93ec69d29e415fb0c84db9e7eb0d7ef2eba74e
Antyradek <antyradek@gmail.com>
2017-06-01 03:59:53 +0200

    Przestaw wyjścia Vrevelmy na Stamped
    Jesteśmy gotowi do przeprowadzania eksperymentów

--- vrep/vreporter/src/vreporter.cpp
+++ vrep/vreporter/src/vreporter.cpp
@@ -3,10 +3,11 @@ extern "C" {
 }
 #include <iostream>
 #include <ros/ros.h>
+#include <ros/console.h>
 #include <omnivelma_msgs/Vels.h>
 
 #define PORT				19997
-#define LUA_FUN_NAME		"setOmnivelmaVels"
+#define LUA_FUN_NAME		"setVrevelmaVels"
 
 ///Id połączenia z V-Repem
 int clientID = -1;
@@ -17,7 +18,7 @@ void velsCallback(const omnivelma_msgs::Vels::ConstPtr& msg)
 	float wheelValues[4] = {(float)(msg -> fr), (float)(msg -> fl), (float)(msg -> rl), (float)(msg -> rr)};
 	
 	//Połączenie, nazwa modelu, typ skryptu, funkcja, ilość int, wartości, ilość float, wartości, ilość stringów, wartości, długości buforów i bufory do zwracania wartości...
-	int result = simxCallScriptFunction(clientID, "omnivelma", sim_scripttype_childscript, LUA_FUN_NAME, 0, nullptr, 4, wheelValues, 0, nullptr, 0, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, simx_opmode_blocking);
+	int result = simxCallScriptFunction(clientID, "vrevelma", sim_scripttype_childscript, LUA_FUN_NAME, 0, nullptr, 4, wheelValues, 0, nullptr, 0, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, simx_opmode_blocking);
 	if (result != simx_return_ok)
 	{
 		std::cerr << "Błąd wysyłania pakietu" << std::endl;
@@ -35,10 +36,9 @@ int main(int argc, char** argv)
 	clientID = simxStart((simxChar*) "127.0.0.1", PORT, true, true, 2000, 5);
 	if(clientID != 0)
 	{
-		std::cerr << "Błąd łączenia z V-REPem" << std::endl;
-		return -1;
+		ROS_FATAL("Błąd łączenia z V-REPem");
 	}
-	std::cout << "Połączono z V-REPem na porcie " << PORT << std::endl;
+	ROS_DEBUG_STREM("Połączono z V-REPem na porcie " << PORT);
 
 	ros::spin();
 	simxFinish(clientID);
